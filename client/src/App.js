d5c01eb3d2eb3c7f2a35a5432b37a9907d3336b2
+ <joseph.njenga@student.moringaschool.com>
2022-12-05 21:30:39 +0300

    Added App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import React, { useEffect, useState } from "react";
+import { Switch, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import MovieList from "./components/MovieList";
+import Footer from "./components/Footer";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    fetch("/me").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
+  // if (!user) return <Login onLogin={setUser} />
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path="/">
+          <NavBar user={user} setUser={setUser} />
+          <MovieList user={user} setUser={setUser} />
+          <Footer />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+      </Switch>
     </div>
   );
 }
