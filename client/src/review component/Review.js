29a17166e81de70179340ea131a35740da6c8d78
JeremyKungu <jeremikungu27@gmail.com>
2022-12-05 14:54:08 +0300

    added stars

--- client/src/review component/Review.js
+++ client/src/review component/Review.js
@@ -1,38 +1,41 @@
 import React from 'react'
 import {useState} from 'react'
 import {Button, Form, Input} from 'reactstrap';
+import Stars from './Stars';
 
 
 function Review() {
-    const [reviews, setReviews] = useState("");
-    const [value, setValue] = useState("");
-    const onChange = (e) => {
-      setReviews(e.target.value);
-    };
-    const onSubmit = (e) => {
-      e.preventDefault();
-      console.log("Form Submitted");
-      setValue(reviews);
-    };
-    return (
-    <div className="form-container">
-      <h1>Enter your Reviews Here!!</h1>
-    <Form onSubmit={onSubmit}>
-      <Input
-        className="form-control"
-        type="text"
-        placeholder="Enter you review"
-        value={reviews}
-        onChange={onChange}
-      />
-      <br></br>
-      <Button type="submit" className="btn btn-primary">
-        Submit
-      </Button>
-      <div>{value}</div>
-    </Form>
-  </div>
-  );
-  }
+  const [reviews, setReviews] = useState("");
+  const [value, setValue] = useState("");
+  const [star, setStar] = useState();
+  const onChange = (e) => {
+    setReviews(e.target.value);
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form Submitted", star);
+    setValue(reviews);
+  };
+  return (
+  <div className="form-container">
+    <h1>Enter your Reviews Here!!</h1>
+  <Stars setStar={setStar} />
+  <Form onSubmit={onSubmit}>
+    <Input
+      className="form-control"
+      type="text"
+      placeholder="Enter you review"
+      value={reviews}
+      onChange={onChange}
+    />
+    <br></br>
+    <Button type="submit" className="btn btn-primary">
+      Submit
+    </Button>
+    <div>{value}</div>
+  </Form>
+</div>
+);
+}
 
 export default Review
