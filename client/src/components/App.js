98dc74914674e644039f4ff43a60065dd64a6273
+ <joseph.njenga@student.moringaschool.com>
2022-12-06 12:00:18 +0300

    commented out unused components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,8 +2,8 @@ import './App.css'
 import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Home";
-import Login from "./Login";
-import Signup from "./Signup";
+// import Login from "./Login";
+// import Signup from "./Signup";
 
 function App() {
   return (
@@ -11,8 +11,8 @@ function App() {
       <Router>
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+          {/* <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} /> */}
         </Routes>
       </Router>
     </div>
