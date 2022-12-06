7a3a16fdfd410e951fec39369d357fe079b23688
IvanNapsterKE <nguyoivan@gmail.com>
2022-12-06 11:05:44 +0300

    ratings color coded

--- client/src/components/Moviecard.js
+++ client/src/components/Moviecard.js
@@ -2,12 +2,24 @@ import React from "react";
 
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 
+const setVoteClass = (vote) =>{
+
+  if(vote >=7){
+    return 'green'
+}else if (vote >=5){
+    return 'orange'
+}else {
+    return 'red'
+}
+
+}
+
 const Movie = ({ title, poster_path, vote_average, overview }) => (
   <div className="movie">
     <img src={IMG_PATH + poster_path} alt={title} />
     <div className="movie-info">
       <h3>{title}</h3>
-      <span>{vote_average}</span>
+      <span className={`movie-info ${setVoteClass(vote_average)}`}>{vote_average}</span>
     </div>
 
     <div className="overview">
