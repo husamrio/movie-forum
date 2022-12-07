250895f50da2f181ecb2f59dd52922198c949d12
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-12-08 01:05:40 +0300

    movie CRUD and associations

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,22 @@
 class MoviesController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :record_not_found
+
+    def index 
+        render json: Movie.all, status: :ok
+    end
+
+    def show 
+        movie = Movie.find_by(id: params[:id])
+        if movie
+            render json: movie, status: :ok
+        else
+            render json: {error: 'Movie not found'}, status: :not_found
+        end
+    end
+
+    private 
+
+    def record_not_found
+        render json:{error:"Movie not found"}, status: :not_found
+    end
 end
