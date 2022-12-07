695d78126a86a67847bcb5c5a4f33e6498b95376
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-12-08 01:06:55 +0300

    create users and associations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+    # rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity
+
+    def index 
+        render json: User.all, status: :ok
+    end
+
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            render json: user, status: :created
+        else
+            render json: {error: user.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
+    def show
+        user = User.find(params[:id])
+        render json: user
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password_digest)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_many :reviews 
+    has_many :movies, through: :reviews 
+
+    validates :username, presence: true, uniqueness: true
 end
