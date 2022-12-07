98d299b5011240124b6c1084662dad4da0b09d43
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-12-08 01:04:43 +0300

    routing features

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :reviews
-  resources :movies
+  resources :users, only: [:index, :create, :show]
+  resources :reviews, only: [:index, :create, :show, :update, :destroy]
+  resources :movies, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
