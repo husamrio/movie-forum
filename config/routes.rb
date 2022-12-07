7c16a66651c34e4b56f53967836c6d6e55356ae4
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-12-07 05:30:42 +0300

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :users
+  resources :reviews
   resources :movies
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
