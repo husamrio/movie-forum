f21124028854b7e8b15ca3d385a7442ad01b9e24
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-12-08 01:07:55 +0300

    review crud and error handling

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,6 @@
 class Review < ApplicationRecord
+    belongs_to :movie 
+    belongs_to :user 
+
+    validates :star_rating, :numericality => {greater_than_or_equal_to: 1, less_than_or_equal_to: 10} 
 end
