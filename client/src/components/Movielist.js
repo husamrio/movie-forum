6ead17059e255c460b51bda77b0c10a971a0000a
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-12-08 01:03:42 +0300

    list fetch and restructure

--- client/src/components/Movielist.js
+++ client/src/components/Movielist.js
@@ -2,8 +2,6 @@ import Moviecard from "./Moviecard";
 import "./Movie.css";
 import { useEffect, useState } from "react";
 
-const API_URL =
-  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1";
 
 // const SEARCH_API =
 //   'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="';
@@ -12,18 +10,15 @@ function Movielist() {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    fetch(API_URL)
+    fetch("/movies")
       .then((res) => res.json())
-      .then((data) => {
-        setMovies(data.results);
-      });
+      .then(setMovies)
+
   }, []);
 
   return (
     <div className="main">
-      {movies.length > 0 && movies.map((movie) => (
-        <Moviecard key={movie.id} {...movie}/>
-      ))}
+      { movies.map((m) =>  ( <Moviecard key={m.id} props = {m} />)) }
     </div>
   );
 }
