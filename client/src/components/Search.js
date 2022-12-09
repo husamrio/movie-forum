b7543a2a2ac153feea6879001a871740920f9c1b
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-12-10 01:42:19 +0300

    onchange functionality

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,48 +1,11 @@
 import React from "react";
-import { useEffect, useState } from "react";
-// import Movielist from "./Movielist";
 
-function Search() {
-    const [movies, setMovies] = useState([]);
-
-//     return (
-//         <div className="searchbar">
-//             <input 
-//                 onChange={searchInput} 
-//                 type="text"
-//                 placeholder="Search for a show, movie..."
-//             />
-//         </div>
-//     );
-useEffect(() => {
-    const fetchMovies = async ()=>{
-        const result = await fetch("/movies")
-        const resultJson = await result.json();
-  
-        setMovies(resultJson)
-    }
-    fetchMovies();
-    },[])
-
-  console.log(movies)
-  const onChangeHandler = (event) => {
-    event.preventDefault();
-  }
-
-
+function Search({ handleSearch }) {
   return (
-    <div className="ui large fluid icon input">
-      <input
-        type="text"
-        placeholder="Search your Recent Transactions"
-        onChange={onChangeHandler}
-        // value= {searching}
-      />
-      <i className="circular search link icon"></i>
+    <div className="searchbar">
+      <input type="text" placeholder="Search tv show, movies, animations..." onChange={handleSearch} />
     </div>
   );
 }
-
-
 
 export default Search;
