8bc13508ee4d7b4969fc18310de25dc59452a6a0
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-12-10 01:46:30 +0300

    props and state variables

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,16 +1,27 @@
 import React from 'react'
+import { useEffect, useState } from "react";
 import Navbar from './Navbar'
 import Movielist from "./Movielist";
 import Footer from "./Footer";
 
-function Home() {
+
+function Home({user, setUser}) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("/movies")
+      .then(res => res.json())
+      .then((m) => setMovies(m))
+  }, []);
+
+
   return (
     <div>
-        <Navbar />
-        <Movielist />
-        <Footer />
+      <Navbar user={user} setUser={setUser} movies={movies} setMovies={setMovies}/>
+      <Movielist user={user} setUser={setUser} movies={movies}/>
+      <Footer />
     </div>
-  )
+  );
 }
 
 export default Home
